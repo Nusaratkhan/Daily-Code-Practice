41ed0b2129631faea4aa50218790b0a017b9ad96
Nusarat Khan <nusaratkhan2005@gmail.com>
2026-02-01 22:46:10 +0530

    Abundant number

---
+++ Nusarat_Abundant23.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int n,sum=0;
+    cin>>n;
+    for(int i=1;i<=n/2;i++){
+        if(n%i==0)
+        sum+=i;
+    }
+
+    if(sum>n)
+    cout<<"Abundant number: "<<n;
+    else
+    cout<<"Not a Abundant number ";
+    return 0;
+
+}
